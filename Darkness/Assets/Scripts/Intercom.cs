37985f4b2e19e5097ac5f218eb2f3e3a30e61dba
Peter <Jellycake567@gmail.com>
2023-09-10 19:34:20 +1000

    intercom script

---
+++ Darkness/Assets/Scripts/Intercom.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intercom : MonoBehaviour
+{
+    int restartCount = 0;
+    AudioClip intercomGreet1;
+    AudioClip intercomGreet2;
+    AudioClip intercomGreet3;
+    AudioClip intercomGreet4;
+    AudioClip intercomGreet5;
+    AudioClip intercomGreet6;
+    AudioClip intercomGreet7;
+    AudioClip intercomGreet8;
+    AudioClip intercomGreet9;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
